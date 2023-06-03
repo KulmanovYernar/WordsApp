df0c14633ba3642f261c5f5bb5b298de606a26ff
yerakulmanov <200103127@stu.sdu.edu.kz>
2023-06-03 21:45:53 +0600

    Create a Preferences DataStore

---
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -0,0 +1,16 @@
+package com.example.wordsapp.data
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+
+private const val LAYOUT_PREFERENCE_NAME = "layout_preferences"
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = LAYOUT_PREFERENCE_NAME
+)
+class SettingsDataStore(context: Context) {
+
+}
